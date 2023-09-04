d4bf06d61bae9250988698eb81328b2567ce7da3
User <User@Harrys-MacBook-Air.local>
2023-09-04 21:53:29 +0100

    Rename project folder and remove uneeded files

--- Morales/Morales/wsgi.py
+++ Morales/Morales/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Morlaes project.
+WSGI config for Morales project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Morlaes.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Morales.settings')
 
 application = get_wsgi_application()
