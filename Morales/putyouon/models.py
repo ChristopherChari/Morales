6bf624ea3634a3e1490d1640506d11189f00c387
christopherchari.dev@gmail.com <christopherchari.dev@gmail.com>
2023-09-05 16:37:09 +0100

    created database model song and added view for it

--- Morales/putyouon/models.py
+++ Morales/putyouon/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Song(models.Model):
+    song_id = models.CharField(max_length=50)
+    title = models.CharField(max_length=100)
+    artist = models.CharField(max_length=100)
+    album = models.CharField(max_length=100)
+    cover_url = models.CharField(max_length=200)
+
--- Morales/putyouon/views.py
+++ Morales/putyouon/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, HttpResponse
-
+from .models import Song
 # Create your views here.
 def home(request):
     return render(request, "home.html")
+
+
+def song_list(request):
+    songs = Song.objects.all()
+    return render(request, 'songs.html', {'songs': songs})
