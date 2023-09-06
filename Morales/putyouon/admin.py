a5d5f211b150a7421c8c11c65e04c2d74134f237
christopherchari.dev@gmail.com <christopherchari.dev@gmail.com>
2023-09-06 22:58:23 +0100

    add like model and functionality

--- Morales/putyouon/admin.py
+++ Morales/putyouon/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Song
+from .models import Like
 
 # Register your models here.
+
+admin.site.register(Like)
 admin.site.register(Song)
--- Morales/putyouon/models.py
+++ Morales/putyouon/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -9,3 +10,11 @@ class Song(models.Model):
     album = models.CharField(max_length=100)
     cover_url = models.CharField(max_length=200)
 
+class Like(models.Model):
+    like_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Connect to the User model
+    song_id = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username} liked {self.song_id} at {self.created_at}'
