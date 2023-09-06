9f8352c187e9bbf865965ef3b1c333f278ebd1ca
User <User@Harrys-MacBook-Air.local>
2023-09-06 22:21:51 +0100

    Add front-end for user authentication

--- Morales/Morales/urls.py
+++ Morales/Morales/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("putyouon.urls"))
+    path("", include("putyouon.urls")),
+    path('accounts/', include('putyouon.urls'))
 ]
--- Morales/putyouon/views.py
+++ Morales/putyouon/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import Song
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import (
+    LoginView, LogoutView, PasswordResetView,
+    PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView,
+)
+from .forms import CustomUserCreationForm 
 # Create your views here.
 def home(request):
     return render(request, "home.html")
@@ -8,3 +17,37 @@ def home(request):
 def song_list(request):
     songs = Song.objects.all()
     return render(request, 'songs.html', {'songs': songs})
+
+# Login view
+class CustomLoginView(LoginView):
+    template_name = 'login.html'
+    authentication_form = AuthenticationForm
+
+# Logout view
+class CustomLogoutView(LogoutView):
+    next_page = 'home'  # Redirect to your home page after logout
+
+# Registration view
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'registration/registration.html', {'form': form})
+
+# Password Reset views (you can customize these as needed)
+class CustomPasswordResetView(PasswordResetView):
+    template_name = 'password_reset.html'
+
+class CustomPasswordResetDoneView(PasswordResetDoneView):
+    template_name = 'password_reset_done.html'
+
+class CustomPasswordResetConfirmView(PasswordResetConfirmView):
+    template_name = 'password_reset_confirm.html'
+
+class CustomPasswordResetCompleteView(PasswordResetCompleteView):
+    template_name = 'password_reset_complete.html'
